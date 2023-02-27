641396a29c080037089d5ebc7c902cee22397f36
Chai Yoon Siong <yoonsiong@gmail.com>
2023-02-27 08:42:24 +0000

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is super interesting");
+        System.out.println("Jenkins is super interesting and amazing");
     }
 
     public String getMessage() {
