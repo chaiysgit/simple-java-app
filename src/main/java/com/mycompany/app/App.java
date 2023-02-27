0c01ccae886ecef8528303d8e82de366b2eff5c4
Chai Yoon Siong <yoonsiong@gmail.com>
2023-02-27 07:56:24 +0000

    Make a change to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is super awesome");
+        System.out.println("Jenkins is super interesting");
     }
 
     public String getMessage() {
